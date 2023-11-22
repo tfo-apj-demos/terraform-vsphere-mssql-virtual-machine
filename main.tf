83b0f7457f218fd6cffb2ea6a45c46948aa253bb
Aaron Evans <aarone@outlook.com.au>
2023-11-22 20:49:03 +1100

    init

---
+++ main.tf
@@ -0,0 +1,44 @@
+data "hcp_packer_image" "mssql-windows-2022" {
+  bucket_name    = "mssql-windows-2022"
+  channel        = "latest"
+  cloud_provider = "vsphere"
+  region         = "Datacenter"
+}
+
+
+module "vm" {
+  source = "github.com/tfo-apj-demos/terraform-vsphere-virtual-machine"
+
+  template          = data.hcp_packer_image.mssql-windows-2022.cloud_image_id
+  hostname          = var.hostname
+  num_cpus          = local.sizes[var.size].cpu
+  memory            = local.sizes[var.size].memory
+  cluster           = local.environments[var.environment]
+  datacenter        = local.sites[var.site]
+  primary_datastore = local.storage_profile[var.storage_profile]
+  resource_pool     = local.tiers[var.tier]
+  tags = {
+    environment      = var.environment
+    site             = var.site
+    backup_policy    = var.backup_policy
+    tier             = var.tier
+    storage_profile  = var.storage_profile
+    security_profile = var.security_profile
+  }
+  folder_path = var.folder_path
+  disk_0_size = 100
+
+  networks = {
+    "seg-general" = "dhcp"
+  }
+
+  userdata = templatefile("${path.module}/templates/userdata.yaml.tmpl", {
+    custom_text = var.custom_text
+    hostname    = var.hostname
+  })
+
+  metadata = templatefile("${path.module}/templates/metadata.yaml.tmpl", {
+    dhcp     = true
+    hostname = var.hostname
+  })
+}
