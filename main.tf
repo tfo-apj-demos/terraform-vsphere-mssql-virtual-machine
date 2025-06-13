2e72a4482a55e7bcb0cfcd1540acbe68fc5b5749
Aaron Evans <aarone@outlook.com.au>
2025-06-13 14:21:49 +1000

    Refactor Terraform configuration to enhance VM provisioning structure and remove redundant variables

--- main.tf
+++ main.tf
@@ -1,47 +1,65 @@
-data "hcp_packer_image" "mssql-windows-2022" {
-  bucket_name    = "mssql-windows-2022"
-  channel        = "latest"
-  cloud_provider = "vsphere"
-  region         = "Datacenter"
-}
+# data "hcp_packer_image" "mssql-windows-2022" {
+#   bucket_name    = "mssql-windows-2022"
+#   channel        = "latest"
+#   cloud_provider = "vsphere"
+#   region         = "Datacenter"
+# }
 
 
-module "vm" {
-  source  = "app.terraform.io/tfo-apj-demos/virtual-machine/vsphere"
-  version = "1.4.2"
+# module "vm" {
+#   source  = "app.terraform.io/tfo-apj-demos/virtual-machine/vsphere"
+#   version = "1.4.2"
 
-  template          = data.hcp_packer_image.mssql-windows-2022.cloud_image_id
-  hostname          = var.hostname
-  num_cpus          = local.sizes[var.size].cpu
-  memory            = local.sizes[var.size].memory
-  cluster           = local.environments[var.environment]
-  datacenter        = local.sites[var.site]
-  primary_datastore = local.storage_profile[var.storage_profile]
-  resource_pool     = local.tiers[var.tier]
+#   template          = data.hcp_packer_image.mssql-windows-2022.cloud_image_id
+#   hostname          = var.hostname
+#   num_cpus          = local.sizes[var.size].cpu
+#   memory            = local.sizes[var.size].memory
+#   cluster           = local.environments[var.environment]
+#   datacenter        = local.sites[var.site]
+#   primary_datastore = local.storage_profile[var.storage_profile]
+#   resource_pool     = local.tiers[var.tier]
 
-  tags = {
-    environment      = var.environment
-    site             = var.site
-    backup_policy    = var.backup_policy
-    tier             = var.tier
-    storage_profile  = var.storage_profile
-    security_profile = var.security_profile
-  }
+#   tags = {
+#     environment      = var.environment
+#     site             = var.site
+#     backup_policy    = var.backup_policy
+#     tier             = var.tier
+#     storage_profile  = var.storage_profile
+#     security_profile = var.security_profile
+#   }
   
-  folder_path = var.folder_path
-  disk_0_size = 100
+#   folder_path = var.folder_path
+#   disk_0_size = 100
 
-  networks = {
-    "seg-general" = "dhcp"
-  }
+#   networks = {
+#     "seg-general" = "dhcp"
+#   }
 
-  userdata = templatefile("${path.module}/templates/userdata.yaml.tmpl", {
-    custom_text = var.custom_text
-    hostname    = var.hostname
-  })
+#   userdata = templatefile("${path.module}/templates/userdata.yaml.tmpl", {
+#     custom_text = var.custom_text
+#     hostname    = var.hostname
+#   })
 
-  metadata = templatefile("${path.module}/templates/metadata.yaml.tmpl", {
-    dhcp     = true
-    hostname = var.hostname
-  })
+#   metadata = templatefile("${path.module}/templates/metadata.yaml.tmpl", {
+#     dhcp     = true
+#     hostname = var.hostname
+#   })
+# }
+
+module "single_virtual_machine" {
+  for_each = var.vm_config
+  source   = "app.terraform.io/tfo-apj-demos/single-virtual-machine/vsphere"
+  version  = "~> 1.3"
+
+  hostname           = each.value.hostname
+  ad_domain          = each.value.ad_domain
+  backup_policy      = each.value.backup_policy
+  environment        = each.value.environment
+  os_type            = each.value.os_type
+  linux_distribution = each.value.linux_distribution
+  security_profile   = each.value.security_profile
+  site               = each.value.site
+  size               = each.value.size
+  storage_profile    = each.value.storage_profile
+  tier               = each.value.tier
 }
--- variables.tf
+++ variables.tf
@@ -1,89 +1,26 @@
-variable "os_type" {
-  description = "The type of operating system to be provisioned"
-  type        = string
-  validation {
-    condition     = var.os_type == "windows" || var.os_type == "linux"
-    error_message = "The os_type must be either 'windows' or 'linux'."
-  }
+# Define a map for VM configuration
+variable "vm_config" {
+  description = "Configuration for multiple VMs"
+  type = map(object({
+    hostname           = string
+    ad_domain          = string
+    backup_policy      = string
+    environment        = string
+    os_type            = string
+    linux_distribution = string
+    security_profile   = string
+    site               = string
+    size               = string
+    storage_profile    = string
+    tier               = string
+  }))
 }
 
-variable "hostname" {
-  description = "The hostname of the VM being provisioned. If left blank a hostname will be generated."
+# Job template ID for the AAP job
+variable "job_template_id" {
+  description = "The ID of the job template to run."
   type        = string
-  default     = ""
 }
 
-variable "size" {
-  description = "T-shirt size for the VM (e.g., small, medium, large)"
-  type        = string
-  validation {
-    condition     = contains(["small", "medium", "large"], var.size)
-    error_message = "Size must be one of 'small', 'medium', or 'large'."
-  }
-}
-
-variable "environment" {
-  description = "The environment of the VM (e.g., dev, test, prod)"
-  type        = string
-  validation {
-    condition     = contains(["dev", "test", "prod"], var.environment)
-    error_message = "Environment must be one of 'dev', 'test', or 'prod'."
-  }
-}
-
-variable "site" {
-  description = "The site or datacenter location for the VM (e.g., sydney, canberra, melbourne)"
-  type        = string
-  validation {
-    condition     = contains(["sydney", "canberra", "melbourne"], var.site)
-    error_message = "Site must be one of 'east', 'west', or 'central'."
-  }
-}
-
-variable "storage_profile" {
-  description = "The storage profile for the VM (e.g., performance, capacity, standard)"
-  type        = string
-  validation {
-    condition     = contains(["performance", "capacity", "standard"], var.storage_profile)
-    error_message = "Storage profile must be one of 'performance', 'capacity', or 'standard'."
-  }
-}
-
-variable "tier" {
-  description = "The resource tier for the VM (e.g., gold, silver, bronze)"
-  type        = string
-  validation {
-    condition     = contains(["gold", "silver", "bronze"], var.tier)
-    error_message = "Tier must be 'gold', 'silver', or 'bronze'."
-  }
-}
-
-variable "security_profile" {
-  description = "The security profile for the VM (e.g., web-server, db-server)"
-  type        = string
-  validation {
-    condition     = contains(["web-server", "db-server"], var.security_profile)
-    error_message = "Security profile must be one of 'web-server' or 'db-server'."
-  }
-}
-
-variable "backup_policy" {
-  description = "The backup policy for the VM (e.g., daily, weekly, monthly)"
-  type        = string
-  validation {
-    condition     = contains(["daily", "weekly", "monthly"], var.backup_policy)
-    error_message = "Backup policy must be one of 'daily', 'weekly', or 'monthly'."
-  }
-}
-
-variable "folder_path" {
-  description = "The path to the VM folder where the virtual machine will be created."
-  type        = string
-  default     = "demo workloads"
-}
-
-variable "custom_text" {
-  description = "Custom text to be rendered in userdata."
-  type        = string
-  default     = "some text to be rendered"
-}
+# TFC Workspace ID for inventory naming
+variable "TFC_WORKSPACE_ID" {}
