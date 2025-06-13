6217dce49e39885bb330ad9eed7142eca74bc96d
Aaron Evans <aarone@outlook.com.au>
2025-06-13 22:10:24 +1000

    Refactor variable definitions in variables.tf by removing unused variables and organizing domain admin credentials

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-# Define a map for VM configuration
 variable "vm_config" {
   description = "Configuration for multiple VMs"
   type = map(object({
@@ -16,11 +15,10 @@ variable "vm_config" {
   }))
 }
 
-# Job template ID for the AAP job
-variable "job_template_id" {
-  description = "The ID of the job template to run."
+variable "domain_admin_user" {
   type        = string
 }
 
-# TFC Workspace ID for inventory naming
-variable "TFC_WORKSPACE_ID" {}
+variable "domain_admin_password" {
+  type        = string
+}
