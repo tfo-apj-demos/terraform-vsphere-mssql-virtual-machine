ff7e366f80e25684a9c3c8450bf792a509eeb733
Aaron Evans <aarone@outlook.com.au>
2025-06-13 15:49:52 +1000

    Comment out output blocks in Terraform configuration for virtual machines

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "virtual_machine_id" {
-  description = "The ID of the virtual machine."
-  value       = module.single_virtual_machine.virtual_machine_id
-}
+# output "virtual_machine_id" {
+#   description = "The ID of the virtual machine."
+#   value       = module.single_virtual_machine.virtual_machine_id
+# }
 
-output "vsphere_compute_cluster_id" {
-  description = "The ID of the vSphere compute cluster where the VM is deployed."
-  value       = module.single_virtual_machine.vsphere_compute_cluster_id
-}
+# output "vsphere_compute_cluster_id" {
+#   description = "The ID of the vSphere compute cluster where the VM is deployed."
+#   value       = module.single_virtual_machine.vsphere_compute_cluster_id
+# }
 
-output "virtual_machine_name" {
-  description = "The name of the virtual machine."
-  value       = module.single_virtual_machine.virtual_machine_name
-}
+# output "virtual_machine_name" {
+#   description = "The name of the virtual machine."
+#   value       = module.single_virtual_machine.virtual_machine_name
+# }
 
-output "ip_address" {
-  description = "The default IP address of the virtual machine."
-  value       = module.single_virtual_machine.ip_address
-}
+# output "ip_address" {
+#   description = "The default IP address of the virtual machine."
+#   value       = module.single_virtual_machine.ip_address
+# }
